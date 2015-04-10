327dc2947f0bdbe10c4d4ce35eef26d0abf0fd70
Guy Ellis <wildfiction@gmail.com>
2015-04-10 15:55:50 -0700

    Add first test

--- test/multi.js
+++ test/multi.js
@@ -1,2 +1,16 @@
 'use strict';
 
+var MultiCache = require('..');
+var assert = require('assert');
+
+describe('Multi Cache',function(){
+  it('should set an object in the local cache only', function(done){
+    var multiCache = new MultiCache({
+      localCache: true,
+      remoteCache: false
+    });
+    multiCache.set('myKey','myValue',function(err,result){
+      done();
+    });
+  });
+});
