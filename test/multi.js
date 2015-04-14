5f37f55463dece0880169e61f9954443ba47dc77
Guy Ellis <wildfiction@gmail.com>
2015-04-14 00:32:30 -0400

    Create caches by name

--- test/multi.js
+++ test/multi.js
@@ -1,26 +1,42 @@
 'use strict';
 
-var NodeCache = require('../lib/cache-lib/node-cache');
+var nodeCache = require('../lib/cache-lib/node-cache');
 var redis = require('../lib/cache-lib/redis');
 var MultiCache = require('..');
 var assert = require('assert');
 var debug = require('debug')('multi:test.multi');
 
 describe('Multi Cache',function(){
-  var localCache;
-  var remoteCache;
-  before(function(done){
-    localCache = new NodeCache();
-    remoteCache = redis;
-    done();
-  });
+
 
   it('should set/get an object in the local cache only', function(done){
+    var localCache = nodeCache();
+    var remoteCache = redis;
     var options = {
       useLocalCache: true,
       useRemoteCache: false
     };
     var multiCache = new MultiCache(localCache, remoteCache, options);
+    multiCache.set('myKey','myValue',function(err,result){
+      assert(!err);
+      assert(result);
+      debug(result);
+      multiCache.get('myKey',function(err,value){
+        assert(!err);
+        debug(value);
+        assert.equal(value.myKey,'myValue');
+        // TODO: Test that key/value is not in remoteCache
+        done();
+      });
+    });
+  });
+
+  it('should set/get an object in the local cache only using cache names instead of objects', function(done){
+    var options = {
+      useLocalCache: true,
+      useRemoteCache: false
+    };
+    var multiCache = new MultiCache('node-cache', 'redis', options);
     multiCache.set('myKey','myValue',function(err,result){
       assert(!err);
       assert(result);
