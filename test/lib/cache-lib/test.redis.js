93ecc73b15d9f618e9885ca81b183b7351e9d692
xqjibz <ccrider@whiterose.net>
2015-04-16 15:37:44 -0400

    refactor tests for redis plugin

--- test/lib/cache-lib/test.redis.js
+++ test/lib/cache-lib/test.redis.js
@@ -2,20 +2,21 @@
 
 var assert = require('assert');
 var sinon = require('sinon');
+var redis = require('redis');
 
 var clientStub = {
-  'get': sinon.stub(),
-  'expire': sinon.stub(),
-  'del': sinon.stub(),
-  'set': sinon.stub()
+
+  'get': sinon.stub().callsArg(1),
+  'expire': sinon.stub().callsArg(2),
+  'del': sinon.stub().callsArg(1),
+  'set': sinon.stub().callsArg(2)
 };
 
-var redisPlugin = require('../../../lib/cache-lib/redis')({'testingHarness': true}, clientStub);
+sinon.stub(redis, 'createClient', function(){
+  return clientStub;
+});
 
-
-clientStub.set.callsArg(2);
-clientStub.expire.callsArg(2);
-clientStub.get.callsArg(1);
+var redisPlugin = require('../../../lib/cache-lib/redis')({});
 
 describe('redis plugin', function(){
 
@@ -26,14 +27,14 @@ describe('redis plugin', function(){
     clientStub.set.reset();
   });
 
-  it('should set a key with no TTL', function(done){
+  it('should call set a key with no TTL', function(done){
     redisPlugin.set('testkey', 'testvalue', function(){
       assert(clientStub.set.callCount === 1);
       done();
     });
   });
 
-  it('should set a key with a TTL', function(done){
+  it('should call set a key with a TTL', function(done){
     redisPlugin.set('testkey', 'testvalue', 1, function(){
       assert(clientStub.set.callCount === 1);
       assert(clientStub.expire.callCount === 1);
@@ -41,9 +42,16 @@ describe('redis plugin', function(){
     });
   });
 
-  it('should get a key', function(done){
+  it('should call get a key', function(done){
     redisPlugin.get('testkey', function(){
       assert(clientStub.get.callCount === 1);
+      done();
+    });
+  });
+
+  it('should call delete a key', function(done){
+    redisPlugin.del('testkey', function(){
+      assert(clientStub.del.callCount === 1);
       done();
     });
   });
